76af19c6e9e5949094dfac97da10350ded0d9b82
HowardTsai891209 <b0922183548@gmail.com>
2020-09-18 00:37:06 +0800

    2020/09/18 change assets

--- WeaponSpin/bin/js/bundle.js
+++ WeaponSpin/bin/js/bundle.js
@@ -7,27 +7,27 @@
         }
         decideWeapon(rotation) {
             rotation %= 360;
-            if (rotation >= 150 && rotation <= 210) {
+            if (rotation > 330 && rotation <= 30) {
                 console.log(1);
                 return;
             }
-            else if (rotation >= 90 && rotation <= 150) {
+            else if (rotation > 30 && rotation <= 90) {
                 console.log(2);
                 return;
             }
-            else if (rotation >= 30 && rotation <= 90) {
+            else if (rotation > 90 && rotation <= 150) {
                 console.log(3);
                 return;
             }
-            else if (rotation >= 330 || rotation <= 30) {
+            else if (rotation > 150 || rotation <= 210) {
                 console.log(4);
                 return;
             }
-            else if (rotation >= 270 && rotation <= 330) {
+            else if (rotation > 210 && rotation <= 270) {
                 console.log(5);
                 return;
             }
-            else if (rotation >= 210 && rotation <= 270) {
+            else if (rotation > 270 && rotation <= 330) {
                 console.log(6);
                 return;
             }
